b30c89cbb98262395ba9ccbc54c89bbe2d37d1e7
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-03-12 20:51:05 +0530

    world

--- src/World.js
+++ src/World.js
@@ -19,6 +19,10 @@ function World() {
             // Work with JSON data here
             console.log(data);
             document.getElementById('total').innerHTML=data.data.total_cases
+            document.getElementById('active').innerHTML=data.data.currently_infected
+            document.getElementById('recover').innerHTML =data.data.recovery_cases
+            document.getElementById('death').innerHTML=data.data.death_cases
+            document.getElementById('date').innerHTML=data.data.last_update
         //     newtotal(data.data.total_cases)
         // newactive(data.data.currently_infected)
         // newdeath(data.data.death_cases)
@@ -43,32 +47,32 @@ function World() {
 
 <div class="dateinfo">
     <pan>World</pan><br />
-    <date>Last Updated on {Date}</date>
+    <date id="date" >Last Updated on fetching..</date>
 </div>
 
 <div class="first"  id="box">
 
     <h2>Confirmed Case-</h2>
-    <h1 id="total">total</h1>
+    <h1 id="total">fetching..</h1>
     <p>No. of Confirmed COVID-19 Cases in World</p>
 </div>
 <div class="second" id="box">
     
     <h2>Active Case-</h2>
-    <h1 id="active">active</h1>
+    <h1 id="active">fetching..</h1>
     <p>No. of Active COVID-19 Cases in World</p>
 </div>
 <div class="third" id="box">
    
     <h2>Recovered Case-</h2>
-    <h1 id="recover">recover</h1>
+    <h1 id="recover">fetching..</h1>
     <p>No. of Recoveries COVID-19 Cases in World</p>
 </div>
 <div class="fourth" id="box">
     
     <h2>Deceased Case-</h2>
     <h1 id="death">
-    death</h1>
+    fetching..</h1>
     <p>No. of Deaths caused by COVID-19 in World</p>
 </div>
 
